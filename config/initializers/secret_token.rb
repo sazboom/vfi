a402adb31308bd2c23e21fa07b554eca1d648ffc
Scott Wachtmann <swachtmann@sazboom.com>
2013-05-13 12:30:20 -0400

    Initial commit, install devise, haml, rspec, and clients#index

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vfi::Application.config.secret_token = 'd16cdb1e250fab4d4be409a7691f22942ce42bc0c7b48ce7e5c88f6dfe10ed9bf4dcd0923c34c04b62cc4ed81b1576327101df511beb6838c0d00e24cd46e6ad'
